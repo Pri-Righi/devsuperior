0b2b948113aa4eacbf1d1be152960b85f6dc75b0
Priscila Righi <pri.robbe@hotmail.com>
2024-11-15 09:25:49 -0300

    criando projeto

---
+++ src/test/java/com/devsuperiors/dslists/DslistsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperiors.dslists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
