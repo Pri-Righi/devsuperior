d7e7f855f8c67d128a2a9a0ecade4a9a1c88874e
Priscila Righi <pri.robbe@hotmail.com>
2024-11-17 14:48:22 -0300

    move games

--- src/main/java/com/devsuperiors/dslists/repositories/GameListRepository.java
+++ src/main/java/com/devsuperiors/dslists/repositories/GameListRepository.java
@@ -2,9 +2,14 @@ package com.devsuperiors.dslists.repositories;
 
 import com.devsuperiors.dslists.entities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
     //responsavel por acessar, consultar e buscar os dados no banco; devolve uma entidade
 
-
+    @Modifying
+    @Query(nativeQuery = true,
+            value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+    void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
 }
--- src/main/java/com/devsuperiors/dslists/services/GameListService.java
+++ src/main/java/com/devsuperiors/dslists/services/GameListService.java
@@ -5,6 +5,7 @@ import com.devsuperiors.dslists.dto.GameListDTO;
 import com.devsuperiors.dslists.dto.GameMinDTO;
 import com.devsuperiors.dslists.entities.Game;
 import com.devsuperiors.dslists.entities.GameList;
+import com.devsuperiors.dslists.projections.GameMinProjection;
 import com.devsuperiors.dslists.repositories.GameListRepository;
 import com.devsuperiors.dslists.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +28,21 @@ public class GameListService {
         List<GameList> result = gameListRepository.findAll();
         List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
         return dto;
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
